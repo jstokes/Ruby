2c966bd47d45081b20db72dc06eed3b1fa888651
Jeff <jeffecu88@gmail.com>
2010-12-25 21:11:20 -0500

    added health club project

---
+++ HealthClub/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HealthClub::Application.config.secret_token = 'cc72b54dd06b5f276a7e81b9c26af6c7ff05d20123cf41a6e884a98add4eb7b15627d034267a46a5c97d007dd8124fa0ea556746e1c9cf89eb7be466188708aa'
